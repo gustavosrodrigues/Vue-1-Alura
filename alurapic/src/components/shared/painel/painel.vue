1867e8a74177750d7937d6bc60966458a2f9df68
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-19 17:14:15 -0300

    Aula 4 - Deixando tudo mais alegre com efeitos

--- alurapic/src/components/shared/painel/painel.vue
+++ alurapic/src/components/shared/painel/painel.vue
@@ -1,10 +1,12 @@
 <template>        
     <div class="painel">
-        <h2 class="painel__titulo" v-on:dblclick="visivel = !visivel">{{ titulo }}</h2>        
-        <div class="painel__corpo" v-show="visivel">
-            <slot>
-            </slot>
-        </div>
+        <h2 class="painel__titulo" v-on:dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <transition name="painel-fade">        
+            <div class="painel__corpo" v-show="visivel">
+                <slot>
+                </slot>
+            </div>
+        </transition>
     </div>    
 </template>
 
@@ -24,28 +26,36 @@ export default {
 <style scoped>
 /* estilo do painel */ 
 
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 0.30em;
-    box-shadow: 5px 5px 10px grey;
-    width: 15em;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+    .painel {
+        padding: 0 auto;
+        border: solid 2px grey;
+        display: inline-block;
+        margin: 0.30em;
+        box-shadow: 5px 5px 10px grey;
+        width: 15em;
+        height: 100%;
+        vertical-align: top;
+        text-align: center;
+    }
 
-  .painel .painel__titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 1em 0;
-    padding: 0.75em;
-    text-transform: uppercase;
-  }
+    .painel .painel__titulo {
+        text-align: center;
+        border: solid 2px;
+        background: lightblue;
+        margin: 0 0 1em 0;
+        padding: 0.75em;
+        text-transform: uppercase;
+    }
 
-  * {
-    box-shadow: 5px 5px 5px;
-  }
+    * {
+        box-shadow: 5px 5px 5px;
+    }
+
+    .painel-fade-enter, .painel-fade-leave-active {
+        opacity: 0;
+    }
+
+    .painel-fade-enter-active, .painel-fade-leave-active {
+        transition: opacity .4s
+    }
 </style>
