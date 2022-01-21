8365e8c7e99a4f808092570e158a43126d00ad20
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-21 11:39:56 -0300

    Aula 5 - Ajustando a aplicação para SPA

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,105 +1,26 @@
 <template>    
     <div class="corpo">
-        <h1 class="centralizado">{{ titulo1 }}</h1>
-        <div class="container">
-            <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pelo título">            
-            <ul class="lista__fotos">
-                <li class="lista__fotos__item" v-for="imagem of imagensComFiltro" :key="imagem.alt">
-                    <meu-painel v-bind:titulo="imagem.titulo">
-                        <imagem-responsiva v-bind:url="imagem.url" v-bind:alt="imagem.titulo"/>
-                    </meu-painel>                                  
-                </li>            
-            </ul>
-        </div>        
+        <p>página principal</p>
     </div>
 </template>
 
 <script>
-import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
-import Painel from './components/shared/painel/Painel.vue';
 
-export default {
-    
-    components: {
-
-        'meu-painel': Painel,
-        'imagem-responsiva': ImagemResponsiva
-    },
-
-    data () {
-        return {
-            titulo1: 'Alurapic',
-            imagens: [],
-            filtro: ''
-        }
-    },
-
-    computed: {
-
-        imagensComFiltro() {
-            if (this.filtro) {
-                let exp = new RegExp(this.filtro.trim() ,'i');
-                return this.imagens.filter(imagem => exp.test(imagem.titulo));
-            } else {
-                return this.imagens;
-            }
-        }
-    },
-
-    created() {
-
-        this.$http.get('http://localhost:3000/v1/fotos')
-            .then(res => res.json())
-            .then(fotos => this.imagens = fotos);
+    export default {    
+        
     }
-}
 </script>
 
 <style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;    
-}
 
-html, .painel {
-    background-color: darkkhaki;
-}
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        width: 80%;
+        max-width: 1200px;
+        margin: 0 auto      
+    }
 
-.corpo {
-    font-family: Helvetica, sans-serif;        
-}
-
-.container {
-    width: 80%;
-    max-width: 1200px;
-    margin: 0 auto
-}
-
-.centralizado {
-    padding: 2em;
-    text-align: center;
-}
-
-.lista__fotos {
-    list-style: none;        
-    /* text-align: center; */
-    
-    display: flex;
-    justify-content: center;    
-    flex-wrap: wrap;
-    
-}
-
-/* .lista__fotos .lista__fotos__item {
-    display: inline-block;
-} */
-
-.lista__fotos__item {
-    margin-bottom: 1rem;   
-}
-
-.filtro {
-    width: 100%;
-}
+    p {
+        text-align: center;
+    }
 </style>
---
+++ alurapic/src/components/home/Home.vue
@@ -0,0 +1,87 @@
+<template>    
+    <div>
+        <h1 class="centralizado">{{ titulo1 }}</h1>    
+        <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pelo título">            
+        <ul class="lista__fotos">
+            <li class="lista__fotos__item" v-for="imagem of imagensComFiltro" :key="imagem.alt">
+                <meu-painel v-bind:titulo="imagem.titulo">
+                    <imagem-responsiva v-bind:url="imagem.url" v-bind:alt="imagem.titulo"/>
+                </meu-painel>                                  
+            </li>            
+        </ul>            
+    </div>
+</template>
+
+<script>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Painel from '../shared/painel/Painel.vue';
+
+export default {
+    
+    components: {
+
+        'meu-painel': Painel,
+        'imagem-responsiva': ImagemResponsiva
+    },
+
+    data () {
+        return {
+            titulo1: 'Alurapic',
+            imagens: [],
+            filtro: ''
+        }
+    },
+
+    computed: {
+
+        imagensComFiltro() {
+            if (this.filtro) {
+                let exp = new RegExp(this.filtro.trim() ,'i');
+                return this.imagens.filter(imagem => exp.test(imagem.titulo));
+            } else {
+                return this.imagens;
+            }
+        }
+    },
+
+    created() {
+
+        this.$http.get('http://localhost:3000/v1/fotos')
+            .then(res => res.json())
+            .then(fotos => this.imagens = fotos);
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;    
+}
+
+html, .painel {
+    background-color: darkkhaki;
+}
+
+.centralizado {
+    padding: 2em;
+    text-align: center;
+}
+
+.lista__fotos {
+    list-style: none;
+    display: flex;
+    justify-content: center;    
+    flex-wrap: wrap;
+    
+}
+
+.lista__fotos__item {
+    margin-bottom: 1rem;   
+}
+
+.filtro {
+    width: 100%;
+}
+</style>
