026cf854352b4f04b0d51aca6bb3121198ab6a37
Gustavo Rodrigues <gustavosrodrigues0@gmail.com>
2022-01-21 15:14:46 -0300

    Aula 5 - O menu da aplicação

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,15 +1,31 @@
 <template>    
     <div class="corpo">
 
+        <nav>
+            <ul>
+                <li v-for='route of routes' :key='route.titulo'>
+                    <router-link :to="route.path ? route.path : '/'">
+                        {{ route.titulo }}                       
+                    </router-link>
+                </li>                
+            </ul>
+        </nav>
         <router-view></router-view>
-        
+
     </div>
 </template>
 
 <script>
-
+    
+    import { routes } from './routes'
     export default {    
         
+        data () {
+
+            return {
+                routes
+            }
+        }
     }
 </script>
 
